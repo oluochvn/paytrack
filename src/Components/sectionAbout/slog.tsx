2703e1a895348847d4fd05115e9d7d73e5eb7840
oluochvn <oluochvnn@gmail.com>
2026-01-12 16:15:29 +0300

    landing page

--- src/Components/sectionAbout/slog.tsx
+++ src/Components/sectionAbout/slog.tsx
@@ -4,8 +4,12 @@ function About(){
     
     return(
         <section>
-            <h3>Track Smarter, Spend Better.</h3>
-            <p>dsbajbvhjbdsja</p>
+            <h3 className='slog'>Track Smarter, Spend Better.</h3>
+            <p className='paraslog'>Easily monitor where your money goes take charge 
+                of your spending. With clear tracking and smart insights,
+                 managing your finances becomes simple and stress-free</p>
+
+                 <button className='btnstart'>start for free</button>
         </section>
     )
 }
